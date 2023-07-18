4e06b18221d96ee0928b83c91e9500f05fd91438
VictorDides <vadides@gmail.com>
2023-07-17 21:55:23 -0400

    avance 17-07

--- TeloVendo/Pagina/models.py
+++ TeloVendo/Pagina/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Publicacion(models.Model):
+    nombre = models.CharField(max_length=254)
+    edad = models.IntegerField()
+    correo = models.EmailField(max_length=254)
+    telefono = models.TextField()
+    direccion = models.CharField(max_length=254)
 
 # Create your models here.
